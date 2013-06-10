4469f4e8d595c89b25a1647c736ea0dc63494b8f
Jared Szechy <jared.szechy@gmail.com>
2013-06-09 22:51:00 -0400

    Initial creation

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EraumotorsportsCom::Application.config.secret_token = '5982cdc0d01aec053d2a64254eea9972adafb6d0a7beeaf47c0ee35a8886a54e96bbe77aeb0cb0005bd136353cb8688f425ace35f9f20c90b8d9974655410245'
