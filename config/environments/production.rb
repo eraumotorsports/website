b0c4756abe86961a75b815aa5363f7c8d1eca464
Jared Szechy <jared.szechy@gmail.com>
2013-06-11 21:12:29 -0400

    re-enable precompile init

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -48,7 +48,7 @@ EraumotorsportsCom::Application.configure do
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
 
-  config.assets.initialize_on_precompile = false
+  #config.assets.initialize_on_precompile = false
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
